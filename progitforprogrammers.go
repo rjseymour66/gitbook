8e96ba2b3a26290bc740b5906d9f8b9ed9ce41c1
rseymour <rjs.github@gmail.com>
2021-10-18 21:35:06 -0400

    commit to repo

--- progitforprogrammers.go
+++ progitforprogrammers.go
@@ -8,4 +8,5 @@ func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("I just added this in the cli clone")
 	fmt.Println("I just added this in the vscode clone")
+	fmt.Println("THis line added to show the staging area")
 }
